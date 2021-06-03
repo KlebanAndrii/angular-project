a6b07ac49d3585bb78fd7600bbe9d5926c85e1d0
KlebanAndrii <smokymolla888@gmail.com>
2021-06-03 13:29:28 +0300

    angular.lesson.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from './components/home/home.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
+import {PostDetailsComponent} from './components/post-details/post-details.component';
+import {DeactivatorService} from "./service/deactivator.service";
+import { CommentsComponent } from './components/comments/comments.component';
+import {ResolveService} from "./service/resolve.service";
+
+
+let routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'users', component: UsersComponent, children: [{path: ':id', component: UserDetailsComponent}]},
+  {path: 'posts', component: PostsComponent, canDeactivate: [DeactivatorService]},
+  {path: 'posts/:id', component: PostDetailsComponent, canActivate: [DeactivatorService]},
+  {path: 'comments', component: CommentsComponent, resolve:{data: ResolveService}},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    HomeComponent,
+    UserDetailsComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent,
+    CommentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
