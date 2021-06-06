db9e036f92afa6f25377db040afa4c29e2dc9d33
KlebanAndrii <smokymolla888@gmail.com>
2021-06-06 15:45:12 +0300

    angular.hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import {RouterModule, Routes} from "@angular/router";
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+let routes: Routes = [
+  {path: 'users', component: UsersComponent, children: [{path:':id', component: UserDetailsComponent}]}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    UserDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
