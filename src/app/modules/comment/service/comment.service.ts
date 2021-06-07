fe6ddc778a4cb43e9d2c8b1d07d78cd8b54cabfb
KlebanAndrii <smokymolla888@gmail.com>
2021-06-07 23:10:25 +0300

    hw.3

---
+++ src/app/modules/comment/service/comment.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Comment} from "../models/Comment";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private url = 'http://jsonplaceholder.typicode.com/comments'
+
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getComments(): Observable<Comment[]>{
+    return this.httpClient.get<Comment[]>(this.url)
+  }
+}
